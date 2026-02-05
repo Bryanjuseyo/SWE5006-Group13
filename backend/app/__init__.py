d8e89d4de84f1e85deb4c7b9d974fe8dd1622c41
Douglas Lim <douglas.lim.yy@gmail.com>
2026-02-05 23:58:31 +0800

    Add initial sql for user management

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from .config import Config
 from .extensions import cors
+from .models import db, bcrypt
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
+
+    db.init_app(app)
+    bcrypt.init_app(app)
 
     from .api.health import bp as health_bp
     app.register_blueprint(health_bp, url_prefix="/api")
